3d5df47cbde30c06fc5f4383062bcd0748d6b8af
Michaelmar7 <michaelmar719@gmail.com>
2023-04-19 15:15:20 +1200

    updated player movement script and added player fire

---
+++ Boss-Fighter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float projectileSpeed = 30f;
+    public float deadZoneL = -60;
+    public float deadZoneR = 60;
+    
+
+    void Start()
+    {
+        rb.velocity = transform.right * projectileSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < deadZoneL || transform.position.x > deadZoneR)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        Debug.Log(hit.name);
+        if (hit.name != "Player")
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+}
